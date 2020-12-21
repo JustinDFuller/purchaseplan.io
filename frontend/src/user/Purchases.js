f7bb3b2740f6ba6df5f0aea983796c6566f899ce
Justin Fuller <justindanielfuller@gmail.com>
2020-12-21 14:44:58 -0500

    Linter

--- frontend/src/user/Purchases.js
+++ frontend/src/user/Purchases.js
@@ -3,74 +3,64 @@ import React, { useContext } from "react";
 import * as User from "./context";
 import * as userapi from "./api";
 import { Card } from "../layout/Card";
-import * as Product from "../product";
 
 export function PurchaseList() {
   const { user, setUser } = useContext(User.Context);
 
   return (
     <div id="purchase-container">
-      {user.purchases().map(
-        (purchase) =>
-          console.log(purchase) || (
-            <Card>
-              <div style={{ width: "20%", display: "inline-block" }}>
-                <img
-                  src={purchase.data.product.data.image}
-                  alt={purchase.data.product.data.description}
-                  style={{ width: "90%", height: "auto" }}
-                />
-              </div>
-              <div style={{ width: "80%", display: "inline-block" }}>
-                <a href={purchase.data.product.data.url} className="text-white">
-                  <h5 className="card-title">
-                    {purchase.data.product.data.name}
-                  </h5>
-                </a>
-                <p className="form-group">
-                  <strong style={{ marginRight: 3 }}>
-                    ${purchase.data.product.data.price}
-                  </strong>
-                  {purchase.data.product.data.description}
-                </p>
-                <div style={{ marginTop: 10 }}>
-                  <strong style={{ marginRight: 3 }}>Available</strong>
-                  <span className="availablity">{purchase.data.date}</span>
-                </div>
-              </div>
-              <div className="order-buttons">
-                {user.purchases().isNotFirst(purchase) && (
-                  <button
-                    type="button"
-                    onClick={() => {
-                      const u = user.setPurchases(
-                        user.purchases().up(purchase)
-                      );
-                      setUser(u);
-                      userapi.put(u);
-                    }}
-                  >
-                    Buy Sooner
-                  </button>
-                )}
-                {user.purchases().isNotLast(purchase) && (
-                  <button
-                    type="button"
-                    onClick={() => {
-                      const u = user.setPurchases(
-                        user.purchases().down(purchase)
-                      );
-                      setUser(u);
-                      userapi.put(u);
-                    }}
-                  >
-                    Buy Later
-                  </button>
-                )}
-              </div>
-            </Card>
-          )
-      )}
+      {user.purchases().map((purchase) => (
+        <Card>
+          <div style={{ width: "20%", display: "inline-block" }}>
+            <img
+              src={purchase.data.product.data.image}
+              alt={purchase.data.product.data.description}
+              style={{ width: "90%", height: "auto" }}
+            />
+          </div>
+          <div style={{ width: "80%", display: "inline-block" }}>
+            <a href={purchase.data.product.data.url} className="text-white">
+              <h5 className="card-title">{purchase.data.product.data.name}</h5>
+            </a>
+            <p className="form-group">
+              <strong style={{ marginRight: 3 }}>
+                ${purchase.data.product.data.price}
+              </strong>
+              {purchase.data.product.data.description}
+            </p>
+            <div style={{ marginTop: 10 }}>
+              <strong style={{ marginRight: 3 }}>Available</strong>
+              <span className="availablity">{purchase.data.date}</span>
+            </div>
+          </div>
+          <div className="order-buttons">
+            {user.purchases().isNotFirst(purchase) && (
+              <button
+                type="button"
+                onClick={() => {
+                  const u = user.setPurchases(user.purchases().up(purchase));
+                  setUser(u);
+                  userapi.put(u);
+                }}
+              >
+                Buy Sooner
+              </button>
+            )}
+            {user.purchases().isNotLast(purchase) && (
+              <button
+                type="button"
+                onClick={() => {
+                  const u = user.setPurchases(user.purchases().down(purchase));
+                  setUser(u);
+                  userapi.put(u);
+                }}
+              >
+                Buy Later
+              </button>
+            )}
+          </div>
+        </Card>
+      ))}
     </div>
   );
 }
