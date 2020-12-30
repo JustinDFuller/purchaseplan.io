d925d35d5030bc36527e050825bc6da93de86e57
Justin Fuller <justindanielfuller@gmail.com>
2020-12-30 14:16:29 -0500

    Vertical cards

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,4 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./bootstrap-overrides.css";
+import "./bootstrap-overrides.scss";
 
 import React from "react";
 import ReactDOM from "react-dom";
--- frontend/src/user/Purchases.js
+++ frontend/src/user/Purchases.js
@@ -6,39 +6,62 @@ export const Purchases = withContext(function ({ user, setUser }) {
   return (
     <div className="row">
       {user.purchases().map((purchase) => (
-        <div className="col-12 col-lg-4" key={purchase.data.product.data.name}>
+        <div className="col-12" key={purchase.data.product.data.name}>
           <Card noBody>
-            <img
-              className="card-img-top"
-              src={purchase.data.product.data.image}
-              alt={purchase.data.product.data.description}
-              onError={(e) => {
-                e.target.onerror = null;
-                e.target.src =
-                  "https://cdn.dribbble.com/users/2046015/screenshots/6015680/08_404.gif"; // TODO: Find another image.
-              }}
-            />
-            <div className="card-body" style={{ position: "relative" }}>
-              <strong
-                style={styles.combine(styles.bubble, {
-                  position: "absolute",
-                  top: -20,
-                })}
-              >
-                ${purchase.data.product.data.price}
-              </strong>
-              <a href={purchase.data.product.data.url} className="text-white">
-                <h5 className="card-title mt-3">
-                  {purchase.data.product.data.name}
-                </h5>
-              </a>
-              <p>
-                {purchase.data.product.data.description.slice(0, 150)}
-                {purchase.data.product.data.description.length > 150 && "..."}
-              </p>
-              <div style={{ marginTop: 10 }}>
-                <strong style={{ marginRight: 3 }}>Ready to buy on:</strong>
-                <span className="availablity">{purchase.data.date}</span>
+            <div className="row">
+              <div className="col-4">
+                <img
+                  className="card-img-top"
+                  src={purchase.data.product.data.image}
+                  alt={purchase.data.product.data.description}
+                  onError={(e) => {
+                    e.target.onerror = null;
+                    e.target.src =
+                      "https://cdn.dribbble.com/users/2046015/screenshots/6015680/08_404.gif"; // TODO: Find another image.
+                  }}
+                />
+              </div>
+              <div className="col-8">
+                <div className="card-body" style={{ position: "relative" }}>
+                  <strong
+                    style={styles.combine(styles.bubble, {
+                      position: "absolute",
+                      top: 15,
+                      left: -70,
+                    })}
+                  >
+                    ${purchase.data.product.data.price}
+                  </strong>
+                  <a
+                    href={purchase.data.product.data.url}
+                    className="text-white"
+                  >
+                    <h5 className="card-title">
+                      {purchase.data.product.data.name}
+                    </h5>
+                  </a>
+                  <p>
+                    {purchase.data.product.data.description.slice(0, 150)}
+                    {purchase.data.product.data.description.length > 150 &&
+                      "..."}
+                  </p>
+                  <div style={{ marginTop: 10 }}>
+                    <strong style={{ marginRight: 3 }}>Ready to buy on:</strong>
+                    <span className="availablity">{purchase.data.date}</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-12">
+                <div
+                  className="d-flex flex-row align-items-center justify-content-end px-2 py-2"
+                  style={{ borderTop: "1px solid rgba(190, 184, 203, 0.25)" }}
+                >
+                  <button type="button" className="btn btn-primary">
+                    Buy Now
+                  </button>
+                </div>
               </div>
             </div>
           </Card>
