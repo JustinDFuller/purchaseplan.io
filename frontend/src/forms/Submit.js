91210deb2f42ca2de562ee8e9f7931d32fa3639b
Justin Fuller <justindanielfuller@gmail.com>
2021-01-02 11:24:30 -0500

    Make sure forms can be submitted with enter

--- frontend/src/forms/Submit.js
+++ frontend/src/forms/Submit.js
@@ -2,18 +2,29 @@ import { ReactComponent as CheckCircle } from "bootstrap-icons/icons/check-circl
 
 import * as styles from "../styles";
 
-export function Submit({ onClick, loading }) {
+export function Submit({ loading }) {
   if (loading) {
     return <div className="spinner-border mt-1" role="status" />;
   }
 
   return (
-    <CheckCircle
-      onClick={onClick}
-      className="mt-2"
-      style={styles.combine(styles.success, styles.pointer)}
-      width={24}
-      height={24}
-    />
+    <button
+      type="submit"
+      style={{
+        border: "none",
+        background: "transparent",
+        width: 24,
+        height: 24,
+        padding: 0,
+        margin: 0,
+      }}
+    >
+      <CheckCircle
+        className="mt-2"
+        style={styles.combine(styles.success, styles.pointer)}
+        width={24}
+        height={24}
+      />
+    </button>
   );
 }
