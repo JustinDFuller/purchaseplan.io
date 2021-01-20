68cd1f19a45e9d9c4ac2dcbf4855a405a81bfefe
Justin Fuller <justindanielfuller@gmail.com>
2021-01-20 04:29:57 -0500

    App engine

--- terraform/dev/app_engine.tf
+++ terraform/dev/app_engine.tf
@@ -3,49 +3,49 @@ resource "google_app_engine_application" "purchase-plan" {
   location_id = "us-central"
 }
 
-resource "google_service_account" "drone-deploy-central-dev" {
-  account_id   = "drone-deploy-central-dev"
-  display_name = "Drone Deploy Central Dev"
+resource "google_service_account" "drone-central-dev" {
+  account_id   = "drone-central-dev"
+  display_name = "Drone Central Dev"
   description  = "This service account has permissions for drone in purchase-plan-central-dev"
 }
 
-resource "google_organization_iam_custom_role" "drone-deploy" {
-  role_id     = "drone-deploy"
+resource "google_organization_iam_custom_role" "drone" {
+  role_id     = "drone"
   org_id      = data.google_organization.purchaseplanio.org_id
-  title       = "drone-deploy"
+  title       = "drone"
   description = "All roles needed by drone deployments"
   permissions = [
     "storage.objects.delete"
   ]
 }
 
-data "google_iam_policy" "drone-deploy" {
+data "google_iam_policy" "drone" {
   binding {
-    role = "iam.serviceAccountUser"
+    role = "roles/iam.serviceAccountUser"
 
-    members = ["serviceAccount:${google_service_account.drone-deploy-central-dev.email}"]
+    members = ["serviceAccount:${google_service_account.drone-central-dev.email}"]
   }
 
   binding {
-    role = "compute.storageAdmin"
+    role = "roles/compute.storageAdmin"
 
-    members = ["serviceAccount:${google_service_account.drone-deploy-central-dev.email}"]
+    members = ["serviceAccount:${google_service_account.drone-central-dev.email}"]
   }
 
   binding {
-    role = "cloudbuild.builds.editor"
+    role = "roles/cloudbuild.builds.editor"
 
-    members = ["serviceAccount:${google_service_account.drone-deploy-central-dev.email}"]
+    members = ["serviceAccount:${google_service_account.drone-central-dev.email}"]
   }
 
   binding {
-    role = "appengine.deployer"
+    role = "roles/appengine.deployer"
 
-    members = ["serviceAccount:${google_service_account.drone-deploy-central-dev.email}"]
+    members = ["serviceAccount:${google_service_account.drone-central-dev.email}"]
   }
 }
 
-resource "google_service_account_iam_policy" "drone-deploy-central-dev-iam" {
-  service_account_id = google_service_account.drone-deploy-central-dev.id
-  policy_data        = data.google_iam_policy.drone-deploy.policy_data
+resource "google_service_account_iam_policy" "drone-central-dev-iam" {
+  service_account_id = google_service_account.drone-central-dev.id
+  policy_data        = data.google_iam_policy.drone.policy_data
 }
