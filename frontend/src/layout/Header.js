b6732e21a51f56f6d86a450fa8c2fd71e43a6f96
Justin Fuller <justindanielfuller@gmail.com>
2021-05-06 09:38:27 -0400

    Link to blog

--- frontend/src/layout/Header.js
+++ frontend/src/layout/Header.js
@@ -8,10 +8,11 @@ export function Header() {
   return (
     <>
       <div className="alert alert-dark text-center" style={{ borderRadius: 0 }}>
-        Welcome to the Purchase Plan early access. The website may not work perfectly and may change at any time.
+        Welcome to the Purchase Plan early access. The website may not work
+        perfectly and may change at any time.
       </div>
       <nav
-        className="navbar navbark-dark py-3 px-3 m-auto"
+        className="navbar navbar-expand navbark-dark py-3 px-3 m-auto d-flex justify-content-between"
         style={styles.combine(styles.dark, { maxWidth: 1500 })}
       >
         <a className="navbard-brand" href="/">
@@ -21,21 +22,30 @@ export function Header() {
             style={styles.logo}
           />
         </a>
-        <div className="d-flex">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              {auth.state() === Auth.state.LOGGED_IN && (
-                <button
-                  className="btn btn-link nav-link"
-                  style={styles.text}
-                  onClick={async () => setAuth(await auth.logout())}
-                >
-                  Log Out
-                </button>
-              )}
-            </li>
-          </ul>
-        </div>
+        <ul className="navbar-nav float-end">
+          <li className="nav-item">
+            <a
+              className="btn btn-link nav-link"
+              style={styles.text}
+              href="https://medium.com/@purchaseplan.io"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Blog
+            </a>
+          </li>
+          <li className="nav-item">
+            {auth.state() === Auth.state.LOGGED_IN && (
+              <button
+                className="btn btn-link nav-link"
+                style={styles.text}
+                onClick={async () => setAuth(await auth.logout())}
+              >
+                Log Out
+              </button>
+            )}
+          </li>
+        </ul>
       </nav>
     </>
   );
