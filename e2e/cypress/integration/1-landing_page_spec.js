9bf7985dbfc7a5da5b7c619071fd7daa08c25244
Justin Fuller <justindanielfuller@gmail.com>
2021-05-08 09:19:48 -0400

    Drone

--- e2e/cypress/integration/1-landing_page_spec.js
+++ e2e/cypress/integration/1-landing_page_spec.js
@@ -1,16 +1,11 @@
-function url(path) {
-  const base = process.env.TEST_URL || 'http://localhost:3000'
-  return base + path
-}
-
 describe('Landing Page', function () {
   it('Loads', function() {
-    cy.visit(url("/")) 
+    cy.visit("/") 
     cy.contains("Log in")
   })
 
   it("Links to blog", function() {
-    cy.visit(url("/"))
+    cy.visit("/")
 
     cy.get("[data-testid=blog]").should("have.attr",  "href", "https://medium.com/@purchaseplan.io").contains("Blog")
   })
