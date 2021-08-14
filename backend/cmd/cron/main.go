080546058ef70f3236f551760283c83c8467ad20
Justin Fuller <justindanielfuller@gmail.com>
2021-08-14 13:43:37 -0400

    Fix drone pipeline.

--- backend/cmd/cron/main.go
+++ backend/cmd/cron/main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"context"
+	"log"
 
+	"github.com/justindfuller/purchaseplan.io/backend/config"
+	"github.com/justindfuller/purchaseplan.io/backend/datastore"
+)
+
+func main() {
+	ctx := context.Background()
+
+	c, err := config.New()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = datastore.New(ctx, c.GoogleCloudProject)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
