1108fd146a9686cb3d64923392b2aef4c522ec08
Justin Fuller <justindanielfuller@gmail.com>
2021-08-16 09:05:35 -0400

    Test fixture

--- plan/time.go
+++ plan/time.go
@@ -14,6 +14,11 @@ var now = func() *time.Time {
 	return &n
 }
 
+func date(year int, month time.Month, day, hour, minute, second, nsecond int, loc *time.Location) *time.Time {
+	d := time.Date(year, month, day, hour, minute, second, nsecond, loc)
+	return &d
+}
+
 func fromNow(d time.Duration) *time.Time {
 	t := now().Add(d)
 
