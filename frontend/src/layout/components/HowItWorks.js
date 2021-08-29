5fa64def7a3446a387ae4fea9975c5c4b8377fc1
Justin Fuller <justindanielfuller@gmail.com>
2021-08-29 11:10:27 -0400

    Formatting

--- frontend/src/layout/components/HowItWorks.js
+++ frontend/src/layout/components/HowItWorks.js
@@ -5,16 +5,16 @@ export function HowItWorks() {
     <Card light>
       <h5 className="card-title">How It Works</h5>
       <ul className="list-group list-group-flush">
-        <li className="list-group-item">
+        <li className="list-group-item pl-0">
           Edit your savings overview to adjust your plan.
         </li>
-        <li className="list-group-item">
+        <li className="list-group-item pl-0">
           Add a purchase that you're saving for.
         </li>
-        <li className="list-group-item">
+        <li className="list-group-item pl-0">
           Drag and drop purchases in the order you plan to buy.
         </li>
-        <li className="list-group-item">
+        <li className="list-group-item pl-0">
           Purchase plan will remind you when you can buy something!
         </li>
       </ul>
