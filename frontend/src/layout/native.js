efcd5f121a627a2d4b2f8b0ebac37f7bb76c2616
Justin Fuller <justindanielfuller@gmail.com>
2022-01-20 19:09:15 -0500

    Catch all possible errors accessing local storage

--- frontend/src/layout/native.js
+++ frontend/src/layout/native.js
@@ -1,17 +1,17 @@
 export function isNativeApp() {
-  if (window.isNativeApp) {
-    return true;
-  }
-
-  if (!window.localStorage) {
-    return false;
-  }
-
-  if (typeof window.localStorage.getItem !== "function") {
-    return false;
-  }
-
   try {
+    if (window.isNativeApp) {
+      return true;
+    }
+
+    if (!window.localStorage) {
+      return false;
+    }
+
+    if (typeof window.localStorage.getItem !== "function") {
+      return false;
+    }
+
     return window.localStorage.getItem("isNativeApp");
   } catch (e) {
     return false;
