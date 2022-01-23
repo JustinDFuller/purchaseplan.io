e9aacf5a2dc7479fc1f24232000839335dbc4d5b
Justin Fuller <justindanielfuller@gmail.com>
2022-01-23 10:53:20 -0500

    Revert "Playing around with options to override auth urls. Not working. (#41)"
    This reverts commit bd87b153486b738a0cd0f73fdb69071c2556ba3c.

--- mobile/App.js
+++ mobile/App.js
@@ -5,11 +5,10 @@ import { useURL } from "expo-linking";
 import * as SplashScreen from "expo-splash-screen";
 import Svg, { Path } from "react-native-svg";
 import { StatusBar } from "expo-status-bar";
-import * as WebBrowser from "expo-web-browser";
 
 import { useNotifications } from "./useNotifications";
 
-const host = "https://www.purchaseplan.io";
+const host = "http://192.168.86.184:3000";
 const entry = "/app/user/list";
 const defaultURL = host + entry;
 
@@ -17,13 +16,6 @@ const defaultBackgroundColor = "#0a0a1a";
 
 SplashScreen.preventAutoHideAsync().catch(() => {
   /* ignore errors */
-});
-
-Linking.addEventListener("url", function (event) {
-  console.log("linking", event.url);
-  if (event.url.includes("purchaseplan.io")) {
-    WebBrowser.dismissBrowser();
-  }
 });
 
 export default function App() {
@@ -47,9 +39,7 @@ export default function App() {
   }, [tokens, webview]);
 
   const entry = useURL();
-  const [uri, setURI] = useState(
-    entry && entry.includes(host) ? entry : defaultURL
-  );
+  const uri = entry && entry.includes(host) ? entry : defaultURL;
   const isAndroid = Platform.OS === "android";
 
   function handleWebViewLoad() {
@@ -92,33 +82,15 @@ export default function App() {
           onLoad={handleWebViewLoad}
           onError={handleWebViewError}
           onShouldStartLoadWithRequest={(event) => {
-            const auth = ["auth.magic.link/v1/oauth2/google"];
-            if (auth.find((a) => event.url.includes(a))) {
-              WebBrowser.openBrowserAsync(event.url)
-                .then(() => {
-                  setURI("https://www.purchaseplan.io/app/auth/login");
-                })
-                .catch(() => {
-                  WebView.reload();
-                });
-              return false;
-            }
-
-            console.log("Didn't intercept auth", event.url);
-
             const blacklist = ["blog.purchaseplan.io"];
             if (blacklist.find((b) => event.url.includes(b))) {
-              WebBrowser.openBrowserAsync(event.url)
-                .then(console.log)
-                .catch(console.error);
+              Linking.openURL(event.url);
               return false;
             }
 
             const whitelist = ["purchaseplan.io", "magic.link"];
             if (!whitelist.find((w) => event.url.includes(w))) {
-              WebBrowser.openBrowserAsync(event.url)
-                .then(console.log)
-                .catch(console.error);
+              Linking.openURL(event.url);
               return false;
             }
 
