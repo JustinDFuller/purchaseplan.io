bf48bfd38d6ffb7ad8370667be4d16470231143f
Justin Fuller <justindanielfuller@gmail.com>
2022-01-23 22:13:14 -0500

    Grab another type of image from amazon

--- backend/parsers.go
+++ backend/parsers.go
@@ -429,5 +429,16 @@ func (parser AmazonParser) Product() (Product, error) {
 		}
 	})
 
+	doc.Find("#imgBlkFront").Each(func(i int, s *goquery.Selection) {
+		if p.Image != "" {
+			return
+		}
+
+		src, ok := s.Attr("src")
+		if ok && src != "" && len(src) < 1500 {
+			p.Image = src
+		}
+	})
+
 	return p, nil
 }
