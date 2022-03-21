a1de8cf28d353f01b44a739f8d3cfaa33a861cb6
Justin Fuller <justindanielfuller@gmail.com>
2022-03-21 16:59:16 -0400

    e2e tests (#121)
    * e2e tests
    
    * Head layout

--- purchase/e2e/cypress/integration/1-landing_page_spec.js
+++ purchase/e2e/cypress/integration/1-landing_page_spec.js
@@ -3,15 +3,7 @@ import { dataTestId } from '../../dom'
 describe('Landing Page', function () {
   it('Loads', function() {
     cy.visit("/") 
-    cy.contains("Log in")
-  })
-
-  it("Links to blog", function() {
-    cy.visit("/")
-
-    dataTestId("blog")
-      .should("have.attr",  "href", "https://blog.purchaseplan.io")
-      .contains("Blog")
+    cy.contains("Get started for free")
   })
 
   it("Logs In", function() {
