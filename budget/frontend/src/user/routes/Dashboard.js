7cd6950a41956a0a131251b50d908b2364e19d39
Justin Fuller <justindanielfuller@gmail.com>
2022-05-17 09:01:38 -0400

    Refactoring user dashboard

--- budget/frontend/src/user/routes/Dashboard.js
+++ budget/frontend/src/user/routes/Dashboard.js
@@ -6,8 +6,19 @@ import * as Category from "category";
 import * as Transaction from "transaction";
 import * as api from "../api";
 
+function Container({ children }) {
+  return (
+    <div className="d-flex row m-auto pt-xl-4 pb-5" style={{ maxWidth: 1500 }}>
+      {children}
+      <Notifications.components.Toasts />
+    </div>
+  );
+}
+
 export const Dashboard = Auth.context.With(function ({ auth }) {
   const { user, setUser } = User.data.Use();
+
+  console.log(JSON.stringify(user, null, 2));
 
   const budget = user.Budgets().last();
 
@@ -25,46 +36,42 @@ export const Dashboard = Auth.context.With(function ({ auth }) {
     return null;
   }
 
+  if (user.Budgets().isEmpty()) {
+    return (
+      <Container>
+        <User.components.SavingsOverview />
+      </Container>
+    );
+  }
+
   return (
-    <>
-      <div
-        className="d-flex row m-auto pt-xl-4 pb-5"
-        style={{ maxWidth: 1500 }}
-      >
-        {user.Budgets().isEmpty() ? (
-          <User.components.SavingsOverview />
-        ) : (
-          <>
-            <div className="col-12 col-xl-4 px-0 px-xl-3">
-              <Budget.components.Overview
-                budget={budget}
-                view={budget.View()}
-                setView={handleViewChange}
-              />
-              <Transaction.components.Card
-                budget={budget}
-                onSubmit={handleUserChange}
-                user={user}
-              />
-            </div>
-            <div className="col-12 col-xl-8 px-0 px-xl-3 pb-4 pb-xl-0">
-              <Category.components.Groups
-                budget={budget}
-                view={budget.View()}
-                user={user}
-                onChange={handleUserChange}
-              />
-              <Category.components.NewGroup
-                budget={budget}
-                user={user}
-                onChange={handleUserChange}
-              />
-            </div>
-          </>
-        )}
+    <Container>
+      <div className="col-12 col-xl-4 px-0 px-xl-3">
+        <Budget.components.Overview
+          budget={budget}
+          view={budget.View()}
+          setView={handleViewChange}
+        />
+        <Transaction.components.Card
+          budget={budget}
+          onSubmit={handleUserChange}
+          user={user}
+        />
       </div>
-      <Notifications.components.Toasts />
-    </>
+      <div className="col-12 col-xl-8 px-0 px-xl-3 pb-4 pb-xl-0">
+        <Category.components.Groups
+          budget={budget}
+          view={budget.View()}
+          user={user}
+          onChange={handleUserChange}
+        />
+        <Category.components.NewGroup
+          budget={budget}
+          user={user}
+          onChange={handleUserChange}
+        />
+      </div>
+    </Container>
   );
 });
 
